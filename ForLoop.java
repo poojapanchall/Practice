f30c00ade869224beaa48161b123acc8f3148b18
Poojaapanchal <poojapanchal61171@gmail.com>
2023-08-19 14:12:42 +0530

    Text Modified

--- ForLoop.java
+++ ForLoop.java
@@ -5,7 +5,7 @@ public class ForLoop {
 	public static void main(String[] args) {
     for(int i=1; i<=3; i++)
     {
-    	System.out.println("I shall not talk in the class");
+    	System.out.println("I shall talk in the class");
     }
    // System.out.println("--->The End<---");
 	}
